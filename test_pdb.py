5e6e95456f0ef0eac83a42059800132866ace599
AlinaGay <gayfullina.a@gmail.com>
2025-02-12 14:45:15 +0100

    Add tests

---
+++ test_pdb.py
@@ -0,0 +1,17 @@
+import pdb
+
+
+def integers_counter(data):
+    integers_found = 0
+    for item in data:
+        if not isinstance(item, bool) and isinstance(item, int):
+            integers_found += 1
+
+    return integers_found
+
+
+def test_counter():
+    data = [False, 1.0, "some_string", 3, True, 1, [], False]
+    #pdb.set_trace()
+    integers = integers_counter(data)
+    assert integers == 2
