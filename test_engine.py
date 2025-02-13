b10c14f571f8bb2f15431f4ae8bed51f6c24ce00
AlinaGay <gayfullina.a@gmail.com>
2025-02-13 12:10:54 +0100

    Add autouse

--- test_engine.py
+++ test_engine.py
@@ -10,13 +10,12 @@ def engine():
     return Engine()
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def start_engine(engine):
     """Fixture launches the engine"""
     engine.is_running = True
 
 
-@pytest.mark.usefixtures('start_engine')
 def test_engine_is_running(engine, start_engine):
     """Test checks the engine launch"""
     assert engine.is_running
