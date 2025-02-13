17876f633d42cd78da77f99656bce56c86daaa3d
AlinaGay <gayfullina.a@gmail.com>
2025-02-13 12:22:59 +0100

    Add scope and yield

--- test_engine.py
+++ test_engine.py
@@ -4,9 +4,10 @@ import pytest
 from engine_class import Engine
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def engine():
     """Fixture returns the sample of class"""
+    print('Engine factory')
     return Engine()
 
 
@@ -14,8 +15,19 @@ def engine():
 def start_engine(engine):
     """Fixture launches the engine"""
     engine.is_running = True
+    print(f'Before test engine.is_running {engine.is_running}') 
+    yield
+    engine.is_running = False
+    print(f'After test engine.is_running {engine.is_running}')
 
 
-def test_engine_is_running(engine, start_engine):
+def test_engine_is_running(engine):
     """Test checks the engine launch"""
+    print('test_engine_is_running')
     assert engine.is_running
+
+
+def test_check_engine_class(engine):
+    """Test checks the class of object"""
+    print('test_check_engine_class')
+    assert isinstance(engine, Engine)
